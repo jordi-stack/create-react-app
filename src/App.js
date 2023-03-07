4c95a71a6c8ef44520c094c49a3ad8c48f58ce7b
PikaPika <anjasroket@gmail.com>
2023-03-07 16:43:53 +0700

    asd

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Cosmos Ecosystem
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://explorer.jordi-stack.site"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Explorer
         </a>
       </header>
     </div>
